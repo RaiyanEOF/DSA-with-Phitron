6c963c835f0215e995ac128d5fa07f8a87d2337b
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-12 16:34:26 +0600

    Create Doubly Link List Full.cpp

---
+++ Module 9/Doubly Link List Full.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public :
+    Node* next;
+    Node* prev;
+    int val;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void insert_at_tail(Node* &head, Node* &tail,int val){
+    Node* newNode = new Node(val);
+    if(head==NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+void insert_at_head(Node* &head, Node* &tail,int val){
+    Node* newNode = new Node(val);
+    if(head==NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+void insert_at_any_pos(Node* &head, Node* &tail, int idx, int val) {
+    if(idx == 0){
+        insert_at_head(head, tail, val);
+        return;  
+    }
+    Node* temp = head;
+    int i = 0;
+    while (temp != NULL && i < idx - 1){
+        temp = temp->next;
+        i++;
+    }
+    if(temp == NULL || temp->next == NULL){
+        insert_at_tail(head, tail, val);
+        return;
+    }
+    Node* newNode = new Node(val);
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+}
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    while(true){
+        int val;
+        cin >> val;
+        if(val==-1) break;
+        insert_at_tail(head,tail,val);
+    }
+    return 0;
+}
