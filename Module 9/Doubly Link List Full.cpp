6fef00303bd16a4d40876144e1f2ee11cb3088a4
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-07-13 15:21:42 +0600

    Update Doubly Link List Full.cpp

--- Module 9/Doubly Link List Full.cpp
+++ Module 9/Doubly Link List Full.cpp
@@ -54,6 +54,60 @@ void insert_at_any_pos(Node* &head, Node* &tail, int idx, int val) {
     temp->next->prev = newNode;
     temp->next = newNode;
 }
+void delete_at_head(Node* &head,Node* &tail){
+    if(head==NULL){
+        return;
+    }
+    Node* deleteNode = head;
+    if(head->next==NULL){
+        head = NULL;
+        tail = NULL;
+    }
+    else{
+        head = head->next;
+        head->prev = NULL;
+    }
+    delete deleteNode;
+}
+void delete_at_tail(Node* &head, Node* &tail){
+    if(head==NULL){
+        return;
+    }
+    Node* deleteNode = tail;
+    if(head==tail){
+        head = NULL;
+        tail = NULL;
+    }
+    else{
+        tail = tail->prev;
+        tail->next = NULL;
+    }
+    delete deleteNode;
+}
+void delete_at_any_pos(Node* &head,Node* &tail,int idx){
+    if(head==NULL){
+        return;
+    }
+    if(idx==0){
+        delete_at_head(head,tail);
+        return;
+    }
+    Node* temp = head;
+    for(int i=1;i<idx;i++){
+        if(temp == NULL) return;
+        temp = temp->next;
+    }
+    if(temp==NULL || temp->next==NULL){
+        delete_at_tail(head,tail);
+        return;
+    }
+    else{
+    Node* deleteNode = temp->next;
+    temp->next = temp->next->next;
+    temp->next->prev = temp;
+    }
+    delete deleteNode;
+}
 int main(){
     Node* head = NULL;
     Node* tail = NULL;
